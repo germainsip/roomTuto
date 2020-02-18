9da233090a6ceb3abe5cfd0bdd5f97fc1aab59f3
Germain SIPIERE <gsipiere@gmail.com>
2020-02-18 16:07:40 +0100

    Repository ok

--- app/src/main/java/com/example/roomtuto/NoteDatabase.java
+++ app/src/main/java/com/example/roomtuto/NoteDatabase.java
@@ -1,27 +1,55 @@
 package com.example.roomtuto;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Entity;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 //TODO: ajouter des commentaires parlants
 
 @Database(entities = {Note.class},version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
+
     private static NoteDatabase instance;
 
-    public abstract NoteDAO noteDAO();
+    public abstract NoteDAO noteDao();
 
     public static synchronized NoteDatabase getInstance(Context context){
         if (instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(),
-                    NoteDatabase.class, "not_database")
+                    NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void>{
+        private NoteDAO noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao();
+        }
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Titre 1", "Description 1", 1));
+            noteDao.insert(new Note("Titre 2", "Description 2", 2));
+            noteDao.insert(new Note("Titre 3", "Description 3", 3));
+            return null;
+        }
     }
 }
